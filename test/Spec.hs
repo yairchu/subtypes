802ef8791f32da83978a6514a3f02c1efc8ee31f
Yair Chuchem <yairchu@gmail.com>
2024-09-07 19:03:14 +0300

    Copy example from generic-lens to test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,39 @@
+{-# LANGUAGE GHC2021 #-}
+
 import Data.Comp.Ops
+
+import GHC.Generics
+
+-- Examples taken from Csongor Kiss's
+-- generic-lens:Data.Generics.Sum.Subtype's documentation
+
+data Animal
+    = Dog Dog
+    | Cat Name Age
+    | Duck Age
+    deriving (Generic, Show)
+
+data FourLeggedAnimal
+    = Dog4 Dog
+    | Cat4 Name Age
+    deriving (Generic, Show)
+
+data Dog = MkDog
+    { name :: Name
+    , age  :: Age
+    } deriving (Generic, Show)
+
+type Name = String
+type Age  = Int
+
+dog, cat, duck :: Animal
+dog = Dog (MkDog "Shep" 3)
+cat = Cat "Mog" 5
+duck = Duck 2
+
+dog4, cat4 :: FourLeggedAnimal
+dog4 = Dog4 (MkDog "Snowy" 4)
+cat4 = Cat4 "Garfield" 6
 
 main :: IO ()
 main = pure ()
